5b34a9fb93a7a38e077cdded5cbb023e780b7e95
george <george@192.168.56.1>
2018-08-07 23:06:32 +0300

    fixed bit packing

--- src/org/george/chunk/BitPacking.java
+++ src/org/george/chunk/BitPacking.java
@@ -1,10 +1,11 @@
 package org.george.chunk;
+
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Iterator;
 
 public class BitPacking implements Iterable<Integer>, Serializable {
-	
+
 	private int[] array;
 	private int index;
 	private int length;
@@ -12,30 +13,33 @@ public class BitPacking implements Iterable<Integer>, Serializable {
 	private final int CELL_SIZE = Integer.SIZE;
 	private final int MASK;
 	private final int NUMBER_CELLS;
-	
+
 	public BitPacking() {
 		length = 4;
 		array = new int[length];
 		index = 0;
 		dataSize = 1;
-		MASK = (int)Math.pow(2, dataSize) - 1;
+		MASK = (int) Math.pow(2, dataSize) - 1;
 		NUMBER_CELLS = CELL_SIZE / dataSize;
 	}
-	
+
 	public BitPacking(int dataSize) {
 		length = 4;
 		array = new int[length];
 		index = 0;
+		// to shift exei provlima pou douleuei se dinameis tou 2
+
 		for (int i = dataSize; i <= CELL_SIZE; i++) {
 			if (CELL_SIZE % i == 0) {
 				this.dataSize = i;
 				break;
 			}
 		}
-		MASK = (int)Math.pow(2, dataSize) - 1;
-		NUMBER_CELLS = CELL_SIZE / dataSize;
+
+		MASK = (int) Math.pow(2, this.dataSize) - 1;
+		NUMBER_CELLS = CELL_SIZE / this.dataSize;
 	}
-	
+
 	public void add(int value) {
 		int arrayIndex = index >> (CELL_SIZE - Integer.numberOfLeadingZeros(NUMBER_CELLS) - 1); // i / NUMBER_CELLS
 		int cellIndex = index & (NUMBER_CELLS - 1); // i % NUMBER_CELLS
@@ -43,13 +47,13 @@ public class BitPacking implements Iterable<Integer>, Serializable {
 			length = length * 2;
 			array = Arrays.copyOf(array, length);
 		}
-		//mask = mask >>> cellIndex * dataSize;
-		//int cell = value << ((CELL_SIZE / dataSize - 1) - cellIndex) * dataSize;
+		// mask = mask >>> cellIndex * dataSize;
+		// int cell = value << ((CELL_SIZE / dataSize - 1) - cellIndex) * dataSize;
 		value = value << cellIndex * dataSize;
 		array[arrayIndex] = array[arrayIndex] | value;
 		index++;
 	}
-	
+
 	public Integer get(int i) {
 		int arrayIndex = i >> (CELL_SIZE - Integer.numberOfLeadingZeros(NUMBER_CELLS) - 1); // i / NUMBER_CELLS
 		int cellIndex = i & (NUMBER_CELLS - 1); // i % NUMBER_CELLS
@@ -59,11 +63,11 @@ public class BitPacking implements Iterable<Integer>, Serializable {
 		value = value >>> cellIndex * dataSize;
 		return value;
 	}
-	
+
 	public int size() {
 		return index;
 	}
-	
+
 	public boolean isEmpty() {
 		return index == 0;
 	}
@@ -72,11 +76,11 @@ public class BitPacking implements Iterable<Integer>, Serializable {
 	public Iterator<Integer> iterator() {
 		return new BitPackingIterator();
 	}
-	
+
 	private class BitPackingIterator implements Iterator<Integer> {
-		
+
 		private int i;
-		
+
 		public BitPackingIterator() {
 			i = 0;
 		}
@@ -92,7 +96,7 @@ public class BitPacking implements Iterable<Integer>, Serializable {
 			i++;
 			return value;
 		}
-		
+
 	}
 
 }
--- src/org/george/chunk/ColumnFactory.java
+++ src/org/george/chunk/ColumnFactory.java
@@ -1,29 +1,29 @@
 package org.george.chunk;
 
 public class ColumnFactory<E extends Comparable<E>> {
-	
+
 	public Column<E> createColumn(ColumnAnalyzer<E> columnAnalyzer) {
 		// rules
 		return null;
 	}
-	
+
 	public Column<E> createColumn(ColumnAnalyzer<E> columnAnalyzer, ColumnType columnType) {
 		Column<E> column;
-		switch (columnType) {	
-			case RLE:
-				column =  new ColumnRle<E>();
-				break;
-			case BITMAP:
-				column = new ColumnBitmap<E>();
-				break;
-			case ROARING:
-				column = new ColumnBitmapRoaring<E>();
-				break;
-			case DELTA:
-				column = (Column<E>) new ColumnDelta();
-				break;
-			default:
-				column = new ColumnPlain<E>();
+		switch (columnType) {
+		case RLE:
+			column = new ColumnRle<E>();
+			break;
+		case BITMAP:
+			column = new ColumnBitmap<E>();
+			break;
+		case ROARING:
+			column = new ColumnBitmapRoaring<E>();
+			break;
+		case DELTA:
+			column = (Column<E>) new ColumnDelta(columnAnalyzer.range(), columnAnalyzer.minDelta() * (-1));
+			break;
+		default:
+			column = new ColumnPlain<E>();
 		}
 		for (E value : columnAnalyzer) {
 			column.add(value);
